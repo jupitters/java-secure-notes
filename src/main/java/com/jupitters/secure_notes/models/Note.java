b66eca7e8c7c447d64ec76c19f8afb3c8b617bf5
jupitters <sampaiobale@gmail.com>
2026-05-25 22:09:46 -0300

    note model

---
+++ src/main/java/com/jupitters/secure_notes/models/Note.java
@@ -0,0 +1,17 @@
+package com.jupitters.secure_notes.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String content;
+
+    private String ownerUsername;
+}

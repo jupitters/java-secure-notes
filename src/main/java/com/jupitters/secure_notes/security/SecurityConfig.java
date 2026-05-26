32e483ce2edaa0784cd156621b6d13a2d1d0521c
jupitters <sampaiobale@gmail.com>
2026-05-25 23:15:05 -0300

    security

--- src/main/java/com/jupitters/secure_notes/security/SecurityConfig.java
+++ src/main/java/com/jupitters/secure_notes/security/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
@@ -24,5 +28,27 @@ public class SecurityConfig {
         http.httpBasic(withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        if (!manager.userExists("user1")){
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if (!manager.userExists("admin")){
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}password")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
     }
 }

dc18aa7ff7fc7e92274de18d48d19e213260d4f6
jupitters <sampaiobale@gmail.com>
2026-05-26 15:36:14 -0300

    user model

---
+++ src/main/java/com/jupitters/secure_notes/models/User.java
@@ -0,0 +1,90 @@
+package com.jupitters.secure_notes.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users",
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = "username"),
+            @UniqueConstraint(columnNames = "email")
+        })
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @Size(max = 120)
+    @JsonIgnore
+    private String password;
+
+    private boolean acountNonLocked = true;
+    private boolean acountNonExpired = true;
+    private boolean credentialsNonExpired = true;
+    private boolean enabled = true;
+
+    private LocalDate credentialsExpiryDate;
+    private LocalDate accountExpiryDate;
+
+    private String twoFactorSecret;
+    private boolean isTwoFactorEnabled = false;
+    private String signUpMethod;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE})
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id")
+    @JsonBackReference
+    @ToString.Exclude
+    private Role role;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
+
+    public User(String userName, String email, String password) {
+        this.username = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String userName, String email){
+        this.username = userName;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        return userId != null && userId.equals(((User) o).getUserId());
+    }
+
+    @Override
+    public int hashCode() {return getClass().hashCode();}
+
+}

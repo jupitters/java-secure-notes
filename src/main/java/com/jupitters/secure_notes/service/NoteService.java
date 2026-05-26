8a719d5b03bb43bcc5246ae72ec66b127453b49b
jupitters <sampaiobale@gmail.com>
2026-05-25 22:14:30 -0300

    service

--- src/main/java/com/jupitters/secure_notes/service/NoteService.java
+++ src/main/java/com/jupitters/secure_notes/service/NoteService.java
@@ -1,4 +1,7 @@
 package com.jupitters.secure_notes.service;
 
+import com.jupitters.secure_notes.models.Note;
+
 public interface NoteService {
+    Note createNoteForUser(String username, String content);
 }
--- src/main/java/com/jupitters/secure_notes/service/impl/NoteServiceImpl.java
+++ src/main/java/com/jupitters/secure_notes/service/impl/NoteServiceImpl.java
@@ -1,4 +1,22 @@
 package com.jupitters.secure_notes.service.impl;
 
-public class NoteServiceImpl implements NoteService{
+import com.jupitters.secure_notes.models.Note;
+import com.jupitters.secure_notes.repository.NoteRepository;
+import com.jupitters.secure_notes.service.NoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NoteServiceImpl implements NoteService {
+    private final NoteRepository noteRepository;
+
+    @Override
+    public Note createNoteForUser(String username, String content){
+        Note note = new Note();
+        note.setContent(content);
+        note.setOwnerUsername(username);
+
+        return noteRepository.save(note);
+    }
 }

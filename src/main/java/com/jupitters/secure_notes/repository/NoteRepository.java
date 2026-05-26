96f0df1cb7a8513b2e17c94483c3bf9deed7d72c
jupitters <sampaiobale@gmail.com>
2026-05-25 22:20:06 -0300

    note repo

--- src/main/java/com/jupitters/secure_notes/repository/NoteRepository.java
+++ src/main/java/com/jupitters/secure_notes/repository/NoteRepository.java
@@ -3,5 +3,8 @@ package com.jupitters.secure_notes.repository;
 import com.jupitters.secure_notes.models.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByOwnerUsername(String username);
 }

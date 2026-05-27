0eb285bc6bd01d42d9ea19f480213f82e242edba
jupitters <sampaiobale@gmail.com>
2026-05-26 21:49:25 -0300

    user details service

---
+++ src/main/java/com/jupitters/secure_notes/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.jupitters.secure_notes.security.services;
+
+import com.jupitters.secure_notes.models.User;
+import com.jupitters.secure_notes.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        return UserDetailsImpl.build(user);
+    }
+}

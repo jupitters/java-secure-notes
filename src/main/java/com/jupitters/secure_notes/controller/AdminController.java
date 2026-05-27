b0c3723895876809a114deba953dca02d1070bed
jupitters <sampaiobale@gmail.com>
2026-05-26 22:52:19 -0300

    security

--- src/main/java/com/jupitters/secure_notes/controller/AdminController.java
+++ src/main/java/com/jupitters/secure_notes/controller/AdminController.java
@@ -6,12 +6,14 @@ import com.jupitters.secure_notes.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
     @Autowired
     private UserService userService;
@@ -30,7 +32,7 @@ public class AdminController {
     }
 
     @GetMapping("/user/{id}")
-    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+    public ResponseEntity< UserDTO> getUser(@PathVariable Long id) {
         return new ResponseEntity<>(userService.getUserById(id),
                 HttpStatus.OK);
     }

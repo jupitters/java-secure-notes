1047a60709114246c9546caf8dfee3bf69ee50db
jupitters <sampaiobale@gmail.com>
2026-05-26 22:22:41 -0300

    user repo

---
+++ src/main/java/com/jupitters/secure_notes/service/UserService.java
@@ -0,0 +1,15 @@
+package com.jupitters.secure_notes.service;
+
+import com.jupitters.secure_notes.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    void updateUserRole(Long userId, String roleName);
+
+    List<User> getAllUsers();
+
+    UserDTO getUserById(Long id);
+
+    User findByUsername(String username);
+}
---
+++ src/main/java/com/jupitters/secure_notes/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.jupitters.secure_notes.service.impl;
+
+import com.jupitters.secure_notes.models.AppRole;
+import com.jupitters.secure_notes.models.Role;
+import com.jupitters.secure_notes.models.User;
+import com.jupitters.secure_notes.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    public void updateUserRole(Long userId, String roleName) {
+        User user = userRepository.findById(userId).orElseThrow(()
+                -> new RuntimeException("User not found"));
+        AppRole appRole = AppRole.valueOf(roleName);
+        Role role = roleRepository.findByRoleName(appRole)
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+        user.setRole(role);
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+    @Override
+    public UserDTO getUserById(Long id) {
+//        return userRepository.findById(id).orElseThrow();
+        User user = userRepository.findById(id).orElseThrow();
+        return convertToDto(user);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        Optional<User> user = userRepository.findByUserName(username);
+        return user.orElseThrow(() -> new RuntimeException("User not found with username: " + username));
+    }
+}

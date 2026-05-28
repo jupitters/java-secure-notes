9689a5b1bc07bf0c0fe62eb49483979d01ef9813
jupitters <sampaiobale@gmail.com>
2026-05-27 22:52:22 -0300

    security

--- src/main/java/com/jupitters/secure_notes/security/SecurityConfig.java
+++ src/main/java/com/jupitters/secure_notes/security/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -87,5 +88,10 @@ public class SecurityConfig {
                 userRepository.save(admin);
             }
         };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

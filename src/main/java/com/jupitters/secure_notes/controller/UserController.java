fb6152ad98213c26bde3a92d0255ca761d3e376d
jupitters <sampaiobale@gmail.com>
2026-06-01 15:30:58 -0300

    get user

--- src/main/java/com/jupitters/secure_notes/controller/UserController.java
+++ src/main/java/com/jupitters/secure_notes/controller/UserController.java
@@ -1,4 +1,41 @@
 package com.jupitters.secure_notes.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jupitters.secure_notes.dto.UserDTO;
+import com.jupitters.secure_notes.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getUserProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        
+        Map<String, Object> profile = new HashMap<>();
+        profile.put("username", userDetails.getUsername());
+        profile.put("roles", userDetails.getAuthorities().stream()
+                .map(i -> i.getAuthority())
+                .collect(Collectors.toList())
+            );
+
+        return ResponseEntity.ok(profile);
+    }
 }
